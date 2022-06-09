8cbf4c9950a23ae8a20d2e2b1f8e197069781ebc
habib <mdhabib71024@gmail.com>
2022-06-09 15:58:29 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../Home/Home.css'
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar className='header-css' collapseOnSelect expand="lg" variant="dark">
+  <Container>
+  <Navbar.Brand className='ICON-CSS'>HABIB</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    
+    <Nav className='m-auto nav-link'>
+    <Link to={'sdf'}>Home</Link>
+    <Link to={'sdf'}>Home</Link>
+    <Link to={'sdf'}>Home</Link>
+    <Link to={'sdf'}>Home</Link>
+    </Nav>
+    <Nav>
+      <button className='my-btn-css'>Resume</button>
+
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+            
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Home.css';
+import img from '../img/profile-pic (2).png'
+
+const Home = () => {
+    return (
+        <div>
+            <section className='banner-css '>
+               
+
+                
+                
+                    <div className=' text-white p-5 '>
+                    <h1 className='text-white name-css'>Hello <br /> <span> I am Habibullah</span> <br /><h3>Web Developer</h3> </h1>
+                    <p>A front-end+back -end developer with extensive experience in building responsive websites and web 
+applications. <br /> I have a passion for creating user-friendly interfaces and strive to build websites that are both 
+visually appealing and easy to use. <br /> I have a strong 
+understanding of front-end development technologies and principles of user experience design.</p>
+<button className='my-btn-css'>About Me</button>
+
+
+                    </div>
+              
+               
+                
+               
+                
+
+            </section>
+            
+        </div>
+    );
+};
+
+export default Home;
